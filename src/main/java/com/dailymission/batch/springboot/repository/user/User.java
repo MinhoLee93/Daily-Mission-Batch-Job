930ee21caf456c576d01e6af90aa63d922dc31a1
Minho Lee <alsgkgk77@gmail.com>
2020-04-01 14:29:01 +0900

    USER 이메일 Unique Key 삭제

--- src/main/java/com/dailymission/batch/springboot/repository/user/User.java
+++ src/main/java/com/dailymission/batch/springboot/repository/user/User.java
@@ -25,9 +25,10 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-@Table(name = "user", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "email")
-})
+//@Table(name = "user", uniqueConstraints = {
+//        @UniqueConstraint(columnNames = "email")
+//})
+@Table(name = "user")
 @NoArgsConstructor
 public class User extends BaseTimeEntity implements Serializable {
 
